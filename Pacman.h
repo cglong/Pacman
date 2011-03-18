a946e9ca7bfadc900d991d9a632fb9f6f3739cbf
Chris Long <christopherglong@mac.com>
2011-03-18 12:12:28 -0400

    Add Pacman image

---
+++ Pacman.h
@@ -0,0 +1,23 @@
+/*
+ * Exported with BrandonTools v0.9
+ * Invocation command was BrandonTools -mode3 Pacman Pacman.png 
+ * 
+ * Image Information
+ * -----------------
+ * Pacman.png 14@16
+ * 
+ * Quote/Fortune of the Day!
+ * -------------------------
+ * It used to take me all vacation to grow a new hide in place of the one they flogged off me during school term.  ~Mark Twain
+ * 
+ * All bug reports / feature requests are to be sent to Brandon (brandon.whitehead@gatech.edu)
+ */
+
+#ifndef PACMAN_BITMAP_H
+#define PACMAN_BITMAP_H
+
+extern const unsigned short Pacman[224];
+#define PACMAN_WIDTH 14
+#define PACMAN_HEIGHT 16
+
+#endif
