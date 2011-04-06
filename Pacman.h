ddf69d879d6cc85aa3a62f2fd691eec73273b073
Chris Long <christopherglong@mac.com>
2011-04-05 22:10:10 -0400

    Make Pacman into animation

--- Pacman.h
+++ Pacman.h
@@ -1,14 +1,18 @@
 /*
  * Exported with BrandonTools v0.9
- * Invocation command was BrandonTools -mode4 -start=2 -resize=16,16 Pacman Pacman.png 
+ * Invocation command was BrandonTools -mode4 -start=2 Pacman Pacman1.png Pacman2.png Pacman3.png Pacman2.png Pacman1.png 
  * 
  * Image Information
  * -----------------
- * Pacman.png 16@16
+ * Pacman1.png 16@16
+ * Pacman2.png 16@16
+ * Pacman3.png 16@16
+ * Pacman2.png 16@16
+ * Pacman1.png 16@16
  * 
  * Quote/Fortune of the Day!
  * -------------------------
- * I plan on living forever.  So far, so good. ~Author Unknown
+ * A compromise is an agreement whereby both parties get what neither of them wanted.  ~Author Unknown
  * 
  * All bug reports / feature requests are to be sent to Brandon (brandon.whitehead@gatech.edu)
  */
@@ -16,11 +20,29 @@
 #ifndef PACMAN_BITMAP_H
 #define PACMAN_BITMAP_H
 
-extern const unsigned short Pacman[128];
-extern const unsigned short Pacman_palette[32];
-#define PACMAN_WIDTH 16
-#define PACMAN_HEIGHT 16
-#define PACMAN_PALETTE_SIZE 32
+#define PACMAN_PALETTE_SIZE 40
 #define PACMAN_PALETTE_OFFSET 2
+
+extern const unsigned short Pacman_palette[40];
+
+extern const unsigned short Pacman1[128];
+#define PACMAN1_WIDTH 16
+#define PACMAN1_HEIGHT 16
+
+extern const unsigned short Pacman2[128];
+#define PACMAN2_WIDTH 16
+#define PACMAN2_HEIGHT 16
+
+extern const unsigned short Pacman3[128];
+#define PACMAN3_WIDTH 16
+#define PACMAN3_HEIGHT 16
+
+extern const unsigned short Pacman2[128];
+#define PACMAN2_WIDTH 16
+#define PACMAN2_HEIGHT 16
+
+extern const unsigned short Pacman1[128];
+#define PACMAN1_WIDTH 16
+#define PACMAN1_HEIGHT 16
 
 #endif
--- ghost.h
+++ ghost.h
@@ -1,6 +1,6 @@
 /*
  * Exported with BrandonTools v0.9
- * Invocation command was BrandonTools -mode4 -start=34 -resize=16,16 ghost ghost.png 
+ * Invocation command was BrandonTools -mode4 -start=42 -resize=16,16 ghost ghost.png 
  * 
  * Image Information
  * -----------------
@@ -8,7 +8,7 @@
  * 
  * Quote/Fortune of the Day!
  * -------------------------
- * I plan on living forever.  So far, so good. ~Author Unknown
+ * A compromise is an agreement whereby both parties get what neither of them wanted.  ~Author Unknown
  * 
  * All bug reports / feature requests are to be sent to Brandon (brandon.whitehead@gatech.edu)
  */
@@ -17,10 +17,10 @@
 #define GHOST_BITMAP_H
 
 extern const unsigned short ghost[128];
-extern const unsigned short ghost_palette[118];
+extern const unsigned short ghost_palette[126];
 #define GHOST_WIDTH 16
 #define GHOST_HEIGHT 16
-#define GHOST_PALETTE_SIZE 118
-#define GHOST_PALETTE_OFFSET 34
+#define GHOST_PALETTE_SIZE 126
+#define GHOST_PALETTE_OFFSET 42
 
 #endif
